35cb76a871e634d5c64e0a5a55f648d7b42979c0
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-09-10 15:17:52 -0700

    Debugging...

--- firebase/firebaseStorage.js
+++ firebase/firebaseStorage.js
@@ -12,37 +12,62 @@ const config = {
 firebase.initializeApp(config);
 
 // Update 'previous' key with 'current' key values.
-async function setPreviousData() {
-  let ref = firebase.database().ref("/");
-  let data;
+// async function setPreviousData() {
+//   let ref = firebase.database().ref("/data");
+// let data = (await ref.on("value", snapshot => snapshot.val()))
+//   ? await ref.on("value", snapshot => snapshot.val())
+//   : null;
 
-  ref.on("value", snapshot => {
-    data = snapshot.val();
-  });
+//   console.log("===========> Here is the data:", data);
 
-  console.log("===========> Here is the data:", data.data);
+//   firebase
+//     .database()
+//     .ref()
+//     .set({
+//       previous: data
+//     });
+// }
 
-  firebase
-    .database()
-    .ref()
-    .set({
-      previous: data.data
-    });
-}
+const firebaseMethods = {
+  setPreviousData: async () => {
+    let ref = await firebase.database().ref("/data");
+    let prev = (await ref.on("value", snapshot => snapshot.val()))
+      ? await ref.on("value", snapshot => snapshot.val())
+      : null;
+
+    console.log("===========> Here is the data:", prev);
+
+    firebase
+      .database()
+      .ref()
+      .set({
+        previous: prev
+      });
+  },
+
+  writeData: async data => {
+    firebase
+      .database()
+      .ref()
+      .set({
+        data: data
+      });
+  }
+};
 
 // Update Firebase current database
-async function writeData(data) {
-  firebase
-    .database()
-    .ref()
-    .set({
-      data: data
-    });
-}
+// async function writeData(data) {
+//   firebase
+//     .database()
+//     .ref()
+//     .set({
+//       data: data
+//     });
+// }
 
-async function disconnectFirebase() {
-  firebase.database().goOffline();
-  console.log("Disconnected.");
-}
+// async function disconnectFirebase() {
+//   firebase.database().goOffline();
+//   console.log("Disconnected.");
+// }
 
-module.exports = { writeData, disconnectFirebase, setPreviousData };
+module.exports = firebaseMethods;

6448e4be70bbee6a9e4f9ea2f54d3252d02c48bf
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-09-17 21:05:38 -0700

    Expanded color theme.

--- amazon-products/src/constants/colors.js
+++ amazon-products/src/constants/colors.js
@@ -1,16 +1,11 @@
 const colors = {
+  darkPrimary: "#151a1c",
   primary: "#424D57",
   secondary: "#6c7c8c",
   text: "#637382",
+  accent: "#c9f527",
   button: "#3399fe",
   buttonHover: "#208cf7"
 };
-
-// const colors = {
-//   primary: "#576574",
-//   secondary: "#C9F527",
-//   text: "#637382",
-//   button: "#B1B9BF"
-// };
 
 export default colors;

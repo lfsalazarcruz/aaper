509eac0fe7e0142350bec10ed58294653489e3e2
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-10-02 08:36:48 -0700

    Fixing color palette

--- amazon-products/src/constants/colors.js
+++ amazon-products/src/constants/colors.js
@@ -3,13 +3,11 @@ const colors = {
   primary: "#424D57",
   secondary: "#6c7c8c",
   text: "#637382",
-  // accent: "#c9f527",
-  accent: "#FF1493",
-  // button: "#3399fe",
-  buttonHover: "#208cf7",
-  button: "#c9f527"
+  accent: "#c9f527",
+  accent2: "#FF1493",
+  accent3: "#3399fe",
+  accent3Hover: "#208cf7",
+  backgroundColor: "#ffffff"
 };
 
 export default colors;
-
-// accent: "#c9f527",

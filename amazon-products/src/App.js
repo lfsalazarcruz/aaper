1ee59694b1ea12878d3479619d8781d3e85742dc
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-10-02 13:39:22 -0700

    Refactored App component. Created a different Screen component to display the list of products using props.

--- amazon-products/src/App.js
+++ amazon-products/src/App.js
@@ -2,32 +2,13 @@ import React, { Component } from "react";
 import firebase from "firebase";
 import config from "./configurations/firebaseConfig";
 import "./App.css";
-import Home from "./components/home/Home";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import HomeScreen from "./screens/homeScreen/HomeScreen";
+import ListScreen from "./screens/listScreen/ListScreen";
 import {
   SearchbarContainer,
   NavbarTitle,
   SearchInnerContainer,
-  Dropdown,
-  SearchContainer,
-  SearchInput,
-  ProductTable,
-  ProductTableFields,
-  ProductCellPosition,
-  ProductRow,
-  ProductTitleContainer,
-  ProductCellTitle,
-  ProductCellRating,
-  ProductCellReview,
-  ProductCellPrice,
-  ProductCellContainer,
-  ProductCellImage,
-  AmazonLink,
-  CategoryTitle,
-  FieldContainer,
-  ProductPositionContainer,
-  CurrentPosition
+  Dropdown
 } from "./AppStyles";
 
 class App extends Component {
@@ -112,33 +93,6 @@ class App extends Component {
   };
 
   render() {
-    const arrowDirection = number => {
-      if (number > 0) {
-        return (
-          <FontAwesomeIcon icon={faChevronUp} style={{ fontWeight: "bold" }} />
-        );
-      } else if (number < 0) {
-        return (
-          <FontAwesomeIcon
-            icon={faChevronDown}
-            style={{ fontWeight: "bold" }}
-          />
-        );
-      } else {
-        return null;
-      }
-    };
-
-    const getFontColor = number => {
-      if (number > 0) {
-        return "green";
-      } else if (number < 0) {
-        return "red";
-      } else {
-        return;
-      }
-    };
-
     return (
       <div className="App">
         <SearchbarContainer>
@@ -161,79 +115,13 @@ class App extends Component {
           </SearchInnerContainer>
         </SearchbarContainer>
         {!this.state.selectedCategory ? (
-          <Home escalated={this.state.escalated} />
+          <HomeScreen escalated={this.state.escalated} />
         ) : (
-          <>
-            <FieldContainer>
-              <SearchContainer>
-                <CategoryTitle>
-                  Category: {this.state.selectedCategory}
-                </CategoryTitle>
-                <CategoryTitle>
-                  Last Update: {this.state.dateUdpated}
-                </CategoryTitle>
-                <SearchInput
-                  type="text"
-                  name="searchTerm"
-                  onChange={this.filterList}
-                  placeholder="Search item..."
-                />
-              </SearchContainer>
-              <ProductTableFields>
-                <ProductCellPosition>Position</ProductCellPosition>
-                <ProductCellTitle
-                  style={{
-                    textAlign: "center",
-                    width: "40%"
-                  }}
-                >
-                  Product
-                </ProductCellTitle>
-                <ProductCellContainer />
-                <ProductCellRating>Rating</ProductCellRating>
-                <ProductCellReview>Reviews</ProductCellReview>
-                <ProductCellPrice>Price</ProductCellPrice>
-              </ProductTableFields>
-            </FieldContainer>
-            <ProductTable>
-              {this.state.filtered.map((product, index) => {
-                return (
-                  <ProductRow
-                    id={product.place}
-                    key={product.place}
-                    onClick={this.extendProduct}
-                  >
-                    <ProductPositionContainer
-                      style={{ color: getFontColor(product.counter) }}
-                    >
-                      <ProductCellPosition>{product.place}</ProductCellPosition>
-                      {arrowDirection(product.counter)}
-                      {product.counter > 0 ? (
-                        <CurrentPosition>+{product.counter}</CurrentPosition>
-                      ) : product.counter < 0 ? (
-                        <CurrentPosition>{product.counter}</CurrentPosition>
-                      ) : null}
-                    </ProductPositionContainer>
-                    <ProductTitleContainer>
-                      <ProductCellTitle>{product.title}</ProductCellTitle>
-                      <AmazonLink
-                        href={`https://www.amazon.com${product.url}`}
-                        target="_blank"
-                      >
-                        Buy now
-                      </AmazonLink>
-                    </ProductTitleContainer>
-                    <ProductCellContainer>
-                      <ProductCellImage src={product.image} />
-                    </ProductCellContainer>
-                    <ProductCellRating>{product.rating}</ProductCellRating>
-                    <ProductCellReview>{product.reviews}</ProductCellReview>
-                    <ProductCellPrice>{product.price}</ProductCellPrice>
-                  </ProductRow>
-                );
-              })}
-            </ProductTable>
-          </>
+          <ListScreen
+            categorySelected={this.state.selectedCategory}
+            updatedDate={this.state.dateUdpated}
+            filteredData={this.state.filtered}
+          />
         )}
       </div>
     );
